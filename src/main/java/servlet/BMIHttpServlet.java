c95c935b9a83db8f2c5add1bb091a37c865962a9
teacher <teacher@BE-403->
2022-04-25 21:00:46 +0800

    web

--- src/main/java/servlet/BMIHttpServlet.java
+++ src/main/java/servlet/BMIHttpServlet.java
@@ -2,6 +2,8 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,12 +20,28 @@ public class BMIHttpServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		out.println("call by get");
+		// 第一種方式
+		// 案例1: h=170&w=60 得到 170 與 60
+		// 案例2: h=170,171,172&w=60,61,62 得到 170 與 60
+		/*
 		String height = req.getParameter("h"); // 抓到請求參數 h
 		String weight = req.getParameter("w"); // 抓到請求參數 w
 		out.println("<p>");
 		out.println("height = " + height);
 		out.println("<p>");
 		out.println("weight = " + weight);
+		*/
+		// 第二種方式 (支援陣列)
+		// 案例1: h=170&w=60 得到 170 與 60
+		// 案例2: h=170,171,172&w=60,61,62 得到 [170,171,172] 與 [60,61,62]
+		Map<String, String[]> map = req.getParameterMap();
+		String[] heights = map.get("h");
+		String[] weights = map.get("w");
+		out.println("<p>");
+		out.println("heights = " + Arrays.toString(heights));
+		out.println("<p>");
+		out.println("weights = " + Arrays.toString(weights));
+		
 	}
 
 	@Override
