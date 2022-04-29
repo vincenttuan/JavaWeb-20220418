6c42e47ba69841e8db0745b3acf047dc50e1e163
teacher <teacher@BE-403->
2022-04-29 19:20:03 +0800

    web

---
+++ src/main/java/servlet/EmployeeServlet.java
@@ -0,0 +1,16 @@
+package servlet;
+
+/* 
+ * 取得 employee_form.html 所傳來的資料
+ * 呈現方式如下:
+ * 姓名: John  
+ * 年齡: 25
+ * 性別: M
+ * 階級: B
+ * 生日: 1990/05/20
+ * 語言: Java Python
+ * 備註: ...
+*/
+public class EmployeeServlet {
+	
+}
