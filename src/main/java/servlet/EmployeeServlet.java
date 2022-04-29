3da0ace50c0991af3cf6182e7d45fe5d2f0e7857
teacher <teacher@BE-403->
2022-04-29 19:42:24 +0800

    web

--- src/main/java/servlet/EmployeeServlet.java
+++ src/main/java/servlet/EmployeeServlet.java
@@ -1,5 +1,15 @@
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /* 
  * 取得 employee_form.html 所傳來的資料
  * 呈現方式如下:
@@ -11,6 +21,50 @@ package servlet;
  * 語言: Java Python
  * 備註: ...
 */
-public class EmployeeServlet {
+@WebServlet("/servlet/employee")
+public class EmployeeServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		
+		// case 1
+		String empName = req.getParameter("empName");
+		String empAge = req.getParameter("empAge");
+		String empSex = req.getParameter("empSex");
+		String empPos = req.getParameter("empPos");
+		String empBirth = req.getParameter("empBirth");
+		String[] empLang = req.getParameterValues("empLang");
+		String empMemo = req.getParameter("empMemo");
+		out.print("empName = " + empName + "<br />");
+		out.print("empAge = " + empAge + "<br />");
+		out.print("empSex = " + empSex + "<br />");
+		out.print("empPos = " + empPos + "<br />");
+		out.print("empBirth = " + empBirth + "<br />");
+		out.print("empLang = " + Arrays.toString(empLang) + "<br />");
+		out.print("empMemo = " + empMemo + "<br />");
+		
+		out.println("<hr />");
+		
+		// case 2
+		req.getParameterMap()
+				.entrySet()
+				.forEach(e -> out.print(e.getKey() + " = " + Arrays.toString(e.getValue()) + "<br />"));
+		//out.print(req.getParameterNames());
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("請不要直接執行我...");
+	}
 	
 }
