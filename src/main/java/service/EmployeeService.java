bef3e19f6e66270882281ce4eac760f2b0470d4f
teacher <teacher@BE-403->
2022-04-29 20:42:02 +0800

    web

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,5 +1,29 @@
 package service;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import entity.Employee;
+
+// 支援 SingleTon Pattern
 public class EmployeeService {
+	private CopyOnWriteArrayList<Employee> employees = new CopyOnWriteArrayList<Employee>();
+	
+	public static final EmployeeService _instance = new EmployeeService();
+	
+	private EmployeeService() {
+		
+	}
+	
+	public EmployeeService getInstance() {
+		return _instance;
+	}
+	
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+	
+	public CopyOnWriteArrayList<Employee> findAll() {
+		return employees;
+	}
 	
 }
