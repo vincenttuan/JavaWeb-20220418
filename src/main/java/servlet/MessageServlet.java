053604f2c14fc2b1671077322c8e743f39e88bb3
teacher <teacher@BE-403->
2022-05-06 19:08:21 +0800

    web

--- src/main/java/servlet/MessageServlet.java
+++ src/main/java/servlet/MessageServlet.java
@@ -2,11 +2,14 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import service.MessageService;
 
 @WebServlet("/servlet/message")
 public class MessageServlet extends HttpServlet {
@@ -18,7 +21,15 @@ public class MessageServlet extends HttpServlet {
 		resp.setContentType("text/html;charset=utf-8");
 		// 接收前端來的資料
 		String content = req.getParameter("content");
-		
+		// 建立推播服務
+		MessageService messageService = new MessageService();
+		// 進行訊息推播並回傳 httpCode 狀態值
+		int httpCode = messageService.pushMessage(content);
+		// 最後將必要訊息收集並重導給 jsp 去呈現
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/message_result.jsp");
+		req.setAttribute("content", content);
+		req.setAttribute("httpCode", httpCode);
+		rd.forward(req, resp);
 	}
 
 }
