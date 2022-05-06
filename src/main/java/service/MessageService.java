81b7703af354b56c05caf1b3cf8199cc38e74f8c
teacher <teacher@BE-403->
2022-05-06 19:02:29 +0800

    web

---
+++ src/main/java/service/MessageService.java
@@ -0,0 +1,11 @@
+package service;
+
+public class MessageService {
+	public int pushMessage(String content) {
+		// 推播程序 ...
+		// 等一下撰寫
+		int httpCode = 200;
+		// 回應碼
+		return httpCode;
+	}
+}
---
+++ src/main/java/servlet/MessageServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/message")
+public class MessageServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		// 接收前端來的資料
+		String content = req.getParameter("content");
+		
+	}
+
+}
