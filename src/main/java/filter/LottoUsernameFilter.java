35c11e14286bc8ddacf4b0b9d7e1037a419dcecc
teacher <teacher@BE-403->
2022-05-16 19:36:28 +0800

    web

---
+++ src/main/java/filter/LottoUsernameFilter.java
@@ -0,0 +1,28 @@
+package filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = {"/servlet/lotto"}) // 定義要管理的 URL
+public class LottoUsernameFilter extends HttpFilter {
+	
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		req.setCharacterEncoding("utf-8");
+		res.setContentType("text/html;charset=utf-8");
+		res.setCharacterEncoding("utf-8");
+		
+		PrintWriter out = res.getWriter();
+		out.print("我是 LottoUsernameFilter");
+	}
+	
+}
