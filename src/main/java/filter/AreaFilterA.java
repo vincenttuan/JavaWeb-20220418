f6108110e7f39696f0f9581ad6f59032647ebf81
teacher <teacher@BE-403->
2022-05-16 20:07:59 +0800

    web

--- src/main/java/filter/AreaFilterA.java
+++ src/main/java/filter/AreaFilterA.java
@@ -9,7 +9,8 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter("/servlet/area")
+//@WebFilter("/servlet/area")
+// 若要針對同一個 URL 自訂 Filter 的順序, 不可使用 @WebFilter 配置, 必須要在 web.xml 中手動配置
 public class AreaFilterA extends HttpFilter {
 
 	@Override
--- src/main/java/filter/AreaFilterB.java
+++ src/main/java/filter/AreaFilterB.java
@@ -9,13 +9,14 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter("/servlet/area")
+//@WebFilter("/servlet/area")
+//若要針對同一個 URL 自訂 Filter 的順序, 不可使用 @WebFilter 配置, 必須要在 web.xml 中手動配置
 public class AreaFilterB extends HttpFilter {
 
 	@Override
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("AreaFilterBA");
+		System.out.println("AreaFilterB");
 		chain.doFilter(req, res);
 	}
 	
