3c6519f57f346f2acd3b34a3dbe56081765c21f3
teacher <teacher@BE-403->
2022-05-20 21:39:14 +0800

    web

--- src/main/java/servlet/cart/ViewCartServlet.java
+++ src/main/java/servlet/cart/ViewCartServlet.java
@@ -8,12 +8,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/servlet/cart/view")
 public class ViewCartServlet extends HttpServlet {
 	
 	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		HttpSession session = req.getSession(false);
+		if(session == null || session.getAttribute("products") == null) {
+			// 重導到 "/jsp/cart/buy.jsp"
+			RequestDispatcher rd = req.getRequestDispatcher("/jsp/cart/buy.jsp");
+			rd.forward(req, resp);
+			return;
+		}
 		// 重導到 "/jsp/cart/cart.jsp"
 		RequestDispatcher rd = req.getRequestDispatcher("/jsp/cart/cart.jsp");
 		rd.forward(req, resp);
