73d54f5981f39ff40a2cc7b5ab51b9acbdaf4b44
teacher <teacher@BE-403->
2022-05-20 21:41:21 +0800

    web

--- src/main/java/servlet/cart/ClearCartServlet.java
+++ src/main/java/servlet/cart/ClearCartServlet.java
@@ -8,13 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/servlet/cart/clear")
 public class ClearCartServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		HttpSession session = req.getSession(false);
+		if(session != null) {
+			// 強迫 session 失效
+			session.invalidate();
+		}
 		// 重導到 "/jsp/cart/buy.jsp"
 		RequestDispatcher rd = req.getRequestDispatcher("/jsp/cart/buy.jsp");
 		rd.forward(req, resp);
