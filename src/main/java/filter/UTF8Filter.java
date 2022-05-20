0d3c30aab96fd9df0d4431f16d94bab801162e6c
teacher <teacher@BE-403->
2022-05-20 21:32:01 +0800

    web

---
+++ src/main/java/filter/UTF8Filter.java
@@ -0,0 +1,23 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/servlet/*")
+public class UTF8Filter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		req.setCharacterEncoding("UTF-8");
+		res.setCharacterEncoding("UTF-8");
+		chain.doFilter(req, res);
+	}
+	
+}
