b872a76e7e408110c353e55189d96b6d9a1068a8
teacher <teacher@BE-403->
2022-05-23 20:44:38 +0800

    web

--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -3,11 +3,13 @@ package filter;
 import java.io.IOException;
 
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter(urlPatterns = {"/report/*"})
 public class LoginFilter extends HttpFilter {
@@ -15,6 +17,18 @@ public class LoginFilter extends HttpFilter {
 	@Override
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
+		// 1. 判斷是否此人已登入
+		HttpSession session = req.getSession();
+		Object data = session.getAttribute("pass");
+		// 判斷此人是否是尚未登入或登入已過時
+		if(data == null || Boolean.parseBoolean(data+"") != true) { 
+			// 重導到登入頁面
+			RequestDispatcher rd = req.getRequestDispatcher("/servlet/login");
+			rd.forward(req, res);
+		} else {
+			// 通過驗證
+			chain.doFilter(req, res);
+		}
 		
 	}
 	
