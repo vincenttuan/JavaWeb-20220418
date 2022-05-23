b7f458dcf9f08a7c17fb4c597d289bafdc96a023
teacher <teacher@BE-403->
2022-05-23 21:12:38 +0800

    web

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -8,13 +8,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	
 	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/login_form.jsp");
-		rd.forward(req, resp);
+		// 1. 判斷是否此人已登入
+		HttpSession session = req.getSession();
+		Object data = session.getAttribute("pass");
+		if(data == null || Boolean.parseBoolean(data+"") != true) { 		
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/login_form.jsp");
+			rd.forward(req, resp);
+		} else {
+			RequestDispatcher rd = req.getRequestDispatcher("/report/daily");
+			rd.forward(req, resp);
+		}
 	}
 	
 	@Override
