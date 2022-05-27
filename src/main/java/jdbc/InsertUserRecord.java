79f5a5474e81158357543782c13d3237bda16e41
teacher <teacher@BE-403->
2022-05-27 21:13:49 +0800

    w

--- src/main/java/jdbc/InsertUserRecord.java
+++ src/main/java/jdbc/InsertUserRecord.java
@@ -9,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.github.javafaker.Faker;
+
 @WebServlet("/create/user/table")
 public class InsertUserRecord extends BaseServlet {
 
@@ -16,7 +18,7 @@ public class InsertUserRecord extends BaseServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String sql = "insert into user(name, age) values(?, ?)";
 		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
-			pstmt.setString(1, "");
+			pstmt.setString(1, new Faker().name().lastName());
 			pstmt.setInt(2, new Random().nextInt(30) + 10);
 			int rowcount = pstmt.executeUpdate();
 			resp.getWriter().print("rowcount = " + rowcount);
