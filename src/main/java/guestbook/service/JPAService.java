74c8b80263f1fd828229ced6fb86daa481d17f46
teacher <teacher@BE-403->
2022-06-06 19:50:42 +0800

    w

--- src/main/java/guestbook/service/JPAService.java
+++ src/main/java/guestbook/service/JPAService.java
@@ -1,5 +1,31 @@
 package guestbook.service;
 
-public class JPAService {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import guestbook.entity.Guestbook;
+
+public class JPAService {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	
+	public JPAService() {
+		if(emf == null) {
+			emf = Persistence.createEntityManagerFactory("demo");
+		}
+		em = emf.createEntityManager();
+	}
+	
+	public EntityManager getEntityManager() {
+		return em;
+	}
+	
+	public List<Guestbook> queryGuestbook() {
+		Query query = em.createQuery("select g from guestbook g"); // JPQL
+		return query.getResultList();
+	}
 }
