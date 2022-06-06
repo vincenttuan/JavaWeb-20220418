9763e8ba12d405247caed155949c82fe7d358533
teacher <teacher@BE-403->
2022-06-06 21:40:18 +0800

    w

--- src/main/java/guestbook/servlet/UpdateGuestbook.java
+++ src/main/java/guestbook/servlet/UpdateGuestbook.java
@@ -1,5 +1,45 @@
 package guestbook.servlet;
 
-public class UpdateGuestbook {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import guestbook.entity.Guestbook;
+import guestbook.service.JPAService;
+
+@WebServlet("/guestbook/update")
+public class UpdateGuestbook extends HttpServlet {
+	private JPAService service = new JPAService();
+	
+	protected void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 擷取網頁表單內容
+		Integer id = Integer.parseInt(req.getParameter("id"));
+		String username = req.getParameter("username");
+		String content = req.getParameter("content");
+		// 建立 guestbook 物件
+		Guestbook guestbook = new Guestbook();
+		guestbook.setId(id);
+		guestbook.setUsername(username);
+		guestbook.setContent(content);
+		// 修改 guestbook 資料
+		service.updateGuestbook(guestbook);
+		// 重導頁面
+		RequestDispatcher rd = req.getRequestDispatcher("/guestbook/view");
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
 }
