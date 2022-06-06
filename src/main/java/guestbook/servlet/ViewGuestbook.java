b00bb82b5fcd68e0d573c837e4fafba67a0b433b
teacher <teacher@BE-403->
2022-06-06 19:40:46 +0800

    w

--- src/main/java/guestbook/servlet/ViewGuestbook.java
+++ src/main/java/guestbook/servlet/ViewGuestbook.java
@@ -1,5 +1,30 @@
 package guestbook.servlet;
 
-public class ViewGuestbook {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/guestbook/view")
+public class ViewGuestbook extends HttpServlet {
+	
+	protected void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/guestbook_view.jsp");
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+	
 }
