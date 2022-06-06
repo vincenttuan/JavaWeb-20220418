bce5a04301d2bfaf31d5ba4eed658d781cea6ada
teacher <teacher@BE-403->
2022-06-06 20:51:05 +0800

    w

--- src/main/java/guestbook/servlet/AddGuestbook.java
+++ src/main/java/guestbook/servlet/AddGuestbook.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import guestbook.entity.Guestbook;
 import guestbook.service.JPAService;
 
+@WebServlet("/guestbook/add")
 public class AddGuestbook extends HttpServlet {
 	
 	private JPAService service = new JPAService();
