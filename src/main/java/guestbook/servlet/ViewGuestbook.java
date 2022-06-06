626da173c4619f741afb33034708ae8a8fd0698d
teacher <teacher@BE-403->
2022-06-06 19:53:15 +0800

    w

--- src/main/java/guestbook/servlet/ViewGuestbook.java
+++ src/main/java/guestbook/servlet/ViewGuestbook.java
@@ -9,11 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import guestbook.service.JPAService;
+
 @WebServlet("/guestbook/view")
 public class ViewGuestbook extends HttpServlet {
+	private JPAService service = new JPAService();
 	
 	protected void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/guestbook_view.jsp");
+		req.setAttribute("guestbooks", service.queryGuestbook());
 		req.setAttribute("button_name", "add");
 		rd.forward(req, resp);
 	}
