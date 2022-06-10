db35a0fefcaf13ad8bb7e4d15680f26ea4fcc4a3
teacher <teacher@BE-403->
2022-06-10 21:10:48 +0800

    ㄊ

--- src/main/java/rest/PersonRest.java
+++ src/main/java/rest/PersonRest.java
@@ -1,10 +1,13 @@
 package rest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -49,6 +52,20 @@ public class PersonRest extends HttpServlet {
 		req.getParameterMap()
 			.entrySet()
 			.forEach(map -> out.println(map.getKey() + ":" + map.getValue()[0]));
+		//------------------------------------------------------------------------
+		// 自行抓取串流並分解 args
+		ServletInputStream sis = req.getInputStream();
+		InputStreamReader isr = new InputStreamReader(sis);
+		BufferedReader br = new BufferedReader(isr);
+		String args = br.readLine();
+		out.println(args);
+		String[] data = args.split("&");
+		for(String rows : data) {
+			String[] row = rows.split("=");
+			//          name           value
+			out.println(row[0] + "=" + row[1]);
+		}
+		
 	}
 	
 	@Override
@@ -61,6 +78,19 @@ public class PersonRest extends HttpServlet {
 		req.getParameterMap()
 			.entrySet()
 			.forEach(map -> out.println(map.getKey() + ":" + map.getValue()[0]));
+		//------------------------------------------------------------------------
+		// 自行抓取串流並分解 args
+		ServletInputStream sis = req.getInputStream();
+		InputStreamReader isr = new InputStreamReader(sis);
+		BufferedReader br = new BufferedReader(isr);
+		String args = br.readLine();
+		out.println(args);
+		String[] data = args.split("&");
+		for(String rows : data) {
+			String[] row = rows.split("=");
+			//          name           value
+			out.println(row[0] + "=" + row[1]);
+		}
 	}
 
 	
