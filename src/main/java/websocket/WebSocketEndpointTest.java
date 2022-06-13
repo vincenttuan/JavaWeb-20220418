e755d812ed513376b1a3294fa464e0d8b39f114e
teacher <teacher@BE-403->
2022-06-13 19:28:46 +0800

    w

--- src/main/java/websocket/WebSocketEndpointTest.java
+++ src/main/java/websocket/WebSocketEndpointTest.java
@@ -14,23 +14,32 @@ import javax.websocket.server.ServerEndpoint;
 public class WebSocketEndpointTest {
     // 用來存放WebSocket已連接的Socket
     static CopyOnWriteArraySet<Session> sessions;
-    @OnMessage
-    public void onMessage(String message, Session session) throws IOException,
-            InterruptedException, EncodeException {
-    }
+    
     @OnOpen
     public void onOpen(Session session) {
         //紀錄連接到sessions中
         if (sessions == null) {
             sessions = new CopyOnWriteArraySet<Session>();
         }
+        System.out.println("有連線進來: " + session.getId());
         sessions.add(session);
+        System.out.println("目前連線數量: " + sessions.size());
     }
+    
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException,
+            InterruptedException, EncodeException {
+    	
+    }
+    
     @OnClose
     public void onClose(Session session) {
         //將連接從sessions中移除
         if (sessions == null) {
             sessions = new CopyOnWriteArraySet<Session>();
         }
-        sessions.remove(session); }
+        System.out.println("關閉連線: " + session.getId());
+        sessions.remove(session); 
+        System.out.println("目前連線數量: " + sessions.size());
+    }
 }
